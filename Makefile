40ace3aa01b13fb0da036643e3be1463ff99f1c2
iomonad <iomonad@riseup.net>
2025-04-23 10:41:27 +0200

    chore(boilerplate) added skeleton code implementation

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	cc -Wall -Werror -pedantic -O2 afnor_nf_s32_002.c -o afnor_nf_s32_002
---
+++ afnor_nf_s32_002.c
@@ -0,0 +1,44 @@
+/**
+ * Copyright 2025 (c) iomonad <iomonad@riseup.net>
+ *
+ * ----------------------------------------------------------------------------
+ * This is free and unencumbered software released into the public domain.
+ *
+ * Anyone is free to copy, modify, publish, use, compile, sell, or distribute
+ * this software, either in source code form or as a compiled binary, for any
+ * purpose, commercial or non-commercial, and by any means.
+ *
+ * In jurisdictions that recognize copyright laws, the author or authors of
+ * this software dedicate any and all copyright interest in the software to
+ * the public domain. We make this dedication for the benefit of the public
+ * at large and to the detriment of our heirs and successors. We intend this
+ * dedication to be an overt act of relinquishment in perpetuity of all
+ * present and future rights to this software under copyright law.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * ----------------------------------------------------------------------------
+ *
+ * DISCLAIMER:
+ * This software may enable the use of radio frequencies. In France, the use
+ * of radio frequencies or radio equipment outside regulatory conditions is a
+ * criminal offence punishable by up to 6 months' imprisonment and a fine of
+ * up to €30,000. Use of this code for illegitimate or unauthorized radio
+ * transmission is strictly prohibited. It is the user's responsibility to
+ * ensure compliance with all applicable laws and regulations before using
+ * this software for radio frequency operations.
+ *
+ * For more information, consult the ANFR (Agence nationale des fréquences)
+ * and the French Postal and Electronic Communications Code.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+     exit(EXIT_SUCCESS);
+}
